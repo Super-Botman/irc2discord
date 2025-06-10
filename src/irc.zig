481441d3939e8deec1de8aa45aedd38987b00ee2
installer <installer@gmail.com>
2025-06-10 22:15:15 +0200

    complete refacto of the code

--- src/irc.zig
+++ src/irc.zig
@@ -1,162 +1,246 @@
 const std = @import("std");
-const User = @import("user.zig").User;
-const toInt = @import("utils.zig").toInt;
+const tls = @import("tls");
+const utils = @import("utils.zig");
 
-pub const MessageType = enum {
-    command,
-    res,
-    message,
-    undefined,
-};
+const MessageFields = struct {
+    const Self = @This();
 
-pub const Message = struct {
-    type: MessageType,
-    res: ?u64 = null,
-    command: ?Commands = null,
-    args: std.ArrayList([]const u8),
-    server: std.ArrayList(u8),
-    source: std.ArrayList(u8),
-    user: std.ArrayList(u8),
-    data: std.ArrayList(u8),
+    command: std.ArrayList(u8),
+    params: std.ArrayList(std.ArrayList(u8)),
 
-    pub fn init(alloc: std.mem.Allocator) Message {
+    pub fn init(allocator: std.mem.Allocator) Self {
         return .{
-            .type = MessageType.undefined,
-            .args = std.ArrayList([]const u8).init(alloc),
-            .server = std.ArrayList(u8).init(alloc),
-            .source = std.ArrayList(u8).init(alloc),
-            .user = std.ArrayList(u8).init(alloc),
-            .data = std.ArrayList(u8).init(alloc),
+            .command = .init(allocator),
+            .params = .init(allocator),
         };
     }
 
-    pub fn deinit(self: *const Message) void {
+    pub fn deinit(self: *const Self) void {
         self.args.deinit();
-        self.server.deinit();
-        self.source.deinit();
-        self.user.deinit();
-        self.data.deinit();
     }
 };
 
-pub const Commands = enum {
-    JOIN,
-    PRIVMSG,
-    UNDEFINED,
-    PING,
-    NOTICE,
+pub const Message = struct {
+    const Self = @This();
+
+    prefix: std.ArrayList(u8),
+    content: MessageFields,
+
+    pub fn init(allocator: std.mem.Allocator) Self {
+        return .{
+            .prefix = .init(allocator),
+            .content = .init(allocator),
+        };
+    }
+
+    pub fn deinit(self: *const Self) void {
+        self.args.deinit();
+    }
 };
 
-pub fn parse(allocator: std.mem.Allocator, conn: anytype) !std.ArrayList(Message) {
+const Actions = struct {
+    on_message: ?*const fn (msg: Message) anyerror!void = undefined,
+    on_join: ?*const fn (msg: Message) anyerror!void = undefined,
+};
+
+const Settings = struct {
+    host: []const u8,
+    port: u16,
+    user: User,
+    actions: Actions,
+};
+
+const Commands = enum {
+    PING,
+    PRIVMSG,
+};
+
+const Replies = enum(u16) {
+    WHOREPLY = 352,
+    NAMEREPLY = 353,
+};
+
+fn parse(allocator: std.mem.Allocator, buffer: []const u8) !std.ArrayList(Message) {
     var ret = std.ArrayList(Message).init(allocator);
 
-    recv: while (try conn.next()) |data| {
-        var lines = std.mem.splitSequence(u8, data, "\r\n");
-        while (lines.next()) |line| {
-            if (std.mem.eql(u8, line, "")) {
-                break :recv;
-            }
-
-            var msg: Message = Message.init(allocator);
-
-            var parts = std.mem.splitScalar(u8, line, ':');
-            const check = parts.next();
-
-            var headers_buf: ?[]const u8 = undefined;
-            if (check.?.len == 0) {
-                headers_buf = parts.next().?;
-            } else {
-                headers_buf = check;
-            }
-
-            var headers = std.mem.splitScalar(u8, headers_buf.?, ' ');
-
-            var i: u16 = 0;
-            while (headers.next()) |header| : (i += 1) {
-                if (std.mem.eql(u8, header, "")) {
-                    continue;
-                }
-
-                const command = std.meta.stringToEnum(Commands, header);
-                const res = toInt(u64, header) catch null;
-
-                const id_type = u19;
-
-                const isNewCommand = @as(id_type, @intFromBool(command != null));
-                const isCommand = @as(id_type, @intFromBool(msg.type == MessageType.command));
-                const isRes = @as(id_type, @intFromBool(res != null));
-
-                const id: id_type = i | isNewCommand << 16 | isCommand << 17 | isRes << 18;
-                // std.debug.print("id: {b}\n", .{id});
-
-                switch (id) {
-                    0 => {
-                        try msg.server.appendSlice(header);
-                    },
-                    0 | 1 << 16 => {
-                        msg.type = MessageType.command;
-                        msg.command = command;
-                    },
-
-                    1 | 1 << 16 => {
-                        msg.type = MessageType.command;
-                        msg.command = command;
-                    },
-                    1 | 1 << 18 => {
-                        msg.type = MessageType.res;
-                        msg.res = res;
-                    },
-                    1 => {
-                        msg.type = MessageType.message;
-                    },
-
-                    2 | 1 << 17, 2 | 1 << 16 => {
-                        try msg.source.appendSlice(header);
-                    },
-
-                    else => {
-                        try msg.args.append(header);
-                    },
-                }
-            }
-
-            while (parts.next()) |part| {
-                try msg.args.append(part);
-            }
-
-            if (msg.type == MessageType.command) {
-                var iter_server = std.mem.splitScalar(u8, msg.server.items, '@');
-                const user = iter_server.next();
-                const server = iter_server.next();
-                if (user != null and server != null) {
-                    var iter_user = std.mem.splitScalar(u8, user.?, '!');
-                    try msg.user.resize(iter_user.next().?.len);
-                    msg.user.items = @constCast(iter_user.next().?);
-
-                    try msg.server.resize(server.?.len);
-                    msg.server.items = @constCast(server.?);
-                }
-            }
-
-            try ret.append(msg);
+    var lines = std.mem.splitSequence(u8, buffer, "\r\n");
+    while (lines.next()) |line| {
+        if (std.mem.eql(u8, line, "")) {
+            continue;
         }
-        break;
+
+        var msg: Message = Message.init(allocator);
+
+        var message_parts = std.mem.splitScalar(u8, line, ' ');
+        var i: usize = 0;
+        var paramIndex: usize = 0;
+        var isCommand = false;
+        while (message_parts.next()) |part| : (i += 1) {
+            switch (i) {
+                0 => {
+                    if (part[0] == ':') {
+                        try msg.prefix.appendSlice(part);
+                    } else {
+                        isCommand = true;
+                        try msg.content.command.appendSlice(part);
+                    }
+                },
+                1 => {
+                    if (!isCommand) {
+                        try msg.content.command.appendSlice(part);
+                    } else {
+                        var param = std.ArrayList(u8).init(allocator);
+                        try param.appendSlice(part);
+                        try msg.content.params.append(param);
+                    }
+                },
+                else => {
+                    if (paramIndex > 0) {
+                        try msg.content.params.items[paramIndex].appendSlice(" ");
+                        try msg.content.params.items[paramIndex].appendSlice(part);
+                    } else {
+                        var param = std.ArrayList(u8).init(allocator);
+                        try param.appendSlice(part);
+                        try msg.content.params.append(param);
+                    }
+
+                    if (part[0] == ':') {
+                        paramIndex = msg.content.params.items.len - 1;
+                    }
+                },
+            }
+        }
+
+        try ret.append(msg);
     }
 
     return ret;
 }
 
-pub fn login(user: User, conn: anytype) !void {
-    var buf: [std.posix.HOST_NAME_MAX:0]u8 = undefined;
-    const hostname = try std.posix.gethostname(&buf);
-    const writer = conn.writer();
+const User = struct {
+    nickname: []const u8,
+    username: ?[]const u8 = null,
+    realname: ?[]const u8 = null,
+    password: ?[]const u8 = null,
+    channels: []const []const u8,
+};
 
-    std.debug.print("hostname: {s}\n", .{hostname});
+fn login(user: User, connection: *tls.Connection(std.net.Stream)) !void {
+    const writer = connection.writer();
 
     if (user.password != null) {
         try writer.print("PASS {s}\n", .{user.password.?});
     }
     try writer.print("NICK {s}\n", .{user.nickname});
-    try writer.print("USER {s} {s} {s} {s}\n", .{ user.nickname, hostname, user.server, user.realname });
-    try writer.print("JOIN {s}\n", .{user.irc_channel});
+    try writer.print("USER {s} 0 * {s}\n", .{ user.username orelse user.nickname, user.realname orelse user.nickname });
+
+    for (user.channels) |channel| {
+        try writer.print("JOIN {s}\n", .{channel});
+    }
+}
+
+fn callAction(action: ?*const fn (msg: Message) anyerror!void, message: Message) !void {
+    if (action != null) {
+        try action.?(message);
+    }
+    return;
+}
+
+pub const Session = struct {
+    const Self = @This();
+
+    allocator: std.mem.Allocator,
+    connection: tls.Connection(std.net.Stream),
+    settings: Settings,
+    message: Message,
+
+    fn handleCommand(self: *Self, command: Commands) !void {
+        const writer = self.connection.writer();
+        switch (command) {
+            Commands.PING => {
+                try writer.print("PONG {d}", .{std.time.timestamp()});
+            },
+            Commands.PRIVMSG => {
+                callAction(self.settings.actions.on_message, self.message);
+            },
+            else => {
+                unreachable;
+            },
+        }
+    }
+    fn handleReply(self: *Self, reply: Replies) !void {
+        switch (reply) {
+            Replies.NAMEREPLY => {
+                callAction(self.settings.actions.on_join, self.message);
+                return;
+            },
+            else => {
+                unreachable;
+            },
+        }
+    }
+
+    fn handleMessage(self: *Self) void {
+        const replyCode = utils.toInt(u16, self.message.content.command.items) catch 0;
+        if (std.enums.fromInt(Replies, replyCode)) |reply| {
+            self.handleReply(reply) catch std.debug.print("info(irc): Error while handling response\n", .{});
+        } else if (std.meta.stringToEnum(Commands, self.message.content.command.items)) |command| {
+            self.handleCommand(command) catch std.debug.print("info(irc): Error while handling command\n", .{});
+        } else {
+            std.debug.print("prefix: {s}\n", .{self.message.prefix.items});
+            std.debug.print("command: {s}\n", .{self.message.content.command.items});
+            std.debug.print("---\n", .{});
+        }
+    }
+
+    pub fn init(allocator: std.mem.Allocator) Self {
+        return .{
+            .allocator = allocator,
+            .connection = undefined,
+            .settings = undefined,
+            .message = undefined,
+        };
+    }
+
+    pub fn start(self: *Self, settings: Settings) !void {
+        const tcp = std.net.tcpConnectToHost(self.allocator, settings.host, settings.port) catch {
+            std.debug.print("Error: cannot connect to server\n", .{});
+            return;
+        };
+        defer tcp.close();
+
+        var root_ca = try tls.config.cert.fromSystem(self.allocator);
+        defer root_ca.deinit(self.allocator);
+
+        std.debug.print("info(irc): Connected to {s}:{d}\n", .{ settings.host, settings.port });
+
+        var connection = try tls.client(tcp, .{
+            .host = settings.host,
+            .root_ca = root_ca,
+            .insecure_skip_verify = true,
+        });
+        self.connection = connection;
+
+        login(settings.user, &self.connection) catch {
+            @panic("Login failed");
+        };
+
+        self.settings = settings;
+
+        std.debug.print("info(irc): Logged in as {s}\n", .{settings.user.nickname});
+
+        while (try connection.next()) |buffer| {
+            const messages = try parse(self.allocator, buffer);
+            for (messages.items) |message| {
+                self.message = message;
+                self.handleMessage();
+            }
+        }
+
+        _ = connection.close() catch null;
+    }
+};
+
+pub fn init(allocator: std.mem.Allocator) Session {
+    return Session.init(allocator);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,237 +1,102 @@
 const std = @import("std");
-const tls = @import("tls");
-const irc = @import("irc.zig");
 const cache = @import("cache");
-const User = @import("user.zig").User;
-const toInt = @import("utils.zig").toInt;
+const utils = @import("utils.zig");
+const Irc = @import("irc.zig");
 const Discord = @import("discord.zig");
-const Shard = Discord.Shard;
 
-const net = std.net;
-var session: *Discord.Session = undefined;
-var connection: *tls.Connection(net.Stream) = undefined;
-var user: *User = undefined;
-var channelCache: std.StringHashMap(Discord.Channel) = undefined;
+var discordSession: *Discord.Session = undefined;
+var ircSession: *Irc.Session = undefined;
 
-fn getChannel(settings: Discord.CreateGuildChannel) !Discord.Channel {
-    const cachedChannel = channelCache.get(settings.name);
-    var channel = if (cachedChannel != null) cachedChannel else null;
-    const isGoodChannel = if (settings.parent_id != null and channel != null) channel.?.parent_id == settings.parent_id else true;
+const Message = struct {
+    author: []u8,
+    source: []u8,
+    content: []u8,
+};
 
-    if (channel == null or !isGoodChannel) {
-        const res = try session.api.createGuildChannel(Discord.Snowflake.from(user.guild_id), settings);
-        channel = res.value.unwrap();
-    }
-
-    try channelCache.put(settings.name, channel.?);
-    return channel.?;
-}
-
-fn irc_client(allocator: std.mem.Allocator) !void {
-    var categorySettings: Discord.CreateGuildChannel = .{
-        .name = "Channels",
-        .type = .GuildCategory,
+pub fn getenv(envMap: *std.process.EnvMap, name: []const u8) []const u8 {
+    return envMap.get(name) orelse {
+        std.debug.print("{s} var not found", .{name});
+        @panic("exiting");
     };
-    const channelsCat = try getChannel(categorySettings);
-    _ = try getChannel(.{ .name = user.irc_channel[1..], .parent_id = channelsCat.id });
-
-    categorySettings.name = "Users";
-    const queriesCat = try getChannel(categorySettings);
-
-    try irc.login(user.*, connection);
-
-    const writer = connection.writer();
-    try writer.print("WHO {s}\r\n", .{user.irc_channel});
-
-    while (true) {
-        var messages: std.ArrayList(irc.Message) = try irc.parse(allocator, connection);
-        defer messages.deinit();
-
-        for (messages.items) |msg| {
-            std.debug.print("\n---\nNEW MESSAGE\n", .{});
-            std.debug.print("server: {s}\n", .{msg.server.items});
-
-            if (!std.mem.eql(u8, msg.source.items, "")) {
-                std.debug.print("source: {s}\n", .{msg.source.items});
-            }
-
-            if (!std.mem.eql(u8, msg.user.items, "")) {
-                std.debug.print("user: {s}\n", .{msg.user.items});
-            }
-
-            if (msg.res != null) {
-                std.debug.print("response code: {?}\n", .{msg.res});
-                if (msg.res == 352) {
-                    var username: []u8 = @constCast(msg.args.items[5]);
-                    for (username, 0..) |chr, i| {
-                        username[i] = std.ascii.toLower(chr);
-                    }
-                    _ = try getChannel(.{ .name = username, .parent_id = queriesCat.id });
-                }
-            }
-
-            std.debug.print("args: ", .{});
-            for (msg.args.items) |arg| {
-                std.debug.print("{s}, ", .{arg});
-            }
-            std.debug.print("\n", .{});
-
-            if (msg.type == irc.MessageType.command) {
-                std.debug.print("command: {s}\n", .{@tagName(msg.command.?)});
-
-                if (msg.command == irc.Commands.PING) {
-                    try writer.print("PONG :{d}\r\n", .{std.time.timestamp()});
-                }
-
-                if (msg.command == irc.Commands.PRIVMSG) {
-                    const args = msg.args.items;
-                    var discord_message = std.ArrayList(u8).init(allocator);
-                    try std.fmt.format(discord_message.writer(), "**{s}:** {s}", .{ msg.user.items, try std.mem.join(allocator, ":", args) });
-
-                    var message = msg;
-                    const replaced = std.mem.replace(u8, message.source.items, "#", "", message.source.items);
-
-                    message.source.shrinkAndFree(msg.source.items.len - replaced);
-
-                    categorySettings.name = if (replaced != 0) "Channels" else "Users";
-                    const parent = try getChannel(categorySettings);
-                    const name = if (replaced != 0) message.source.items else message.user.items;
-                    const channel = try getChannel(.{
-                        .name = name,
-                        .parent_id = parent.id,
-                        .type = .GuildText,
-                    });
-
-                    var result = try session.api.sendMessage(channel.id, .{ .content = discord_message.items });
-
-                    result.deinit();
-                    discord_message.deinit();
-                }
-            }
-        }
-    }
 }
 
-fn init_client(_: *Shard, payload: Discord.Ready) !void {
-    std.debug.print("logged in as {s}\n", .{payload.user.username});
-
-    const guild = Discord.Snowflake.from(user.guild_id);
-    const channels = try session.api.fetchGuildChannels(guild);
-    for (channels.value.right) |channel| {
-        try channelCache.put(channel.name.?, channel);
-    }
-
-    _ = try std.Thread.spawn(.{}, irc_client, .{session.allocator});
+fn ircMessage(msg: Irc.Message) !void {
+    std.debug.print("{any}\n", .{msg.content});
 }
 
-fn send_msg(_: *Shard, message: Discord.Message) !void {
-    if (message.content != null and message.author.bot == null) {
-        const writer = connection.writer();
-
-        var fetched = try session.api.fetchChannel(message.channel_id);
-        const channel = fetched.value.unwrap();
-        fetched = try session.api.fetchChannel(channel.parent_id.?);
-        const parent = fetched.value.unwrap();
-
-        var dest = std.ArrayList(u8).init(session.allocator);
-        defer dest.deinit();
-        try dest.appendSlice(if (std.mem.eql(u8, parent.name.?, "Channels")) "#" else "");
-        try dest.appendSlice(channel.name.?);
-
-        try writer.print("PRIVMSG {s} :{s}\r\n", .{ dest.items, message.content.? });
-    }
+fn initChannels(msg: Irc.Message) !void {
+    std.debug.print("{any}\n", .{msg.content.params});
 }
 
-fn update_channel(_: *Shard, channel: Discord.Channel) !void {
-    try channelCache.put(channel.name.?, channel);
-}
-
-fn delete_channel(_: *Shard, channel: Discord.Channel) !void {
-    _ = channelCache.remove(channel.name.?);
-}
-
-pub fn main() !void {
-    var gpa: std.heap.GeneralPurposeAllocator(.{}) = .init;
-    const allocator = gpa.allocator();
-    defer _ = gpa.deinit();
-
-    const env_map = try allocator.create(std.process.EnvMap);
-    env_map.* = try std.process.getEnvMap(allocator);
-    defer env_map.deinit();
-
-    user = try User.init(allocator);
-    user.* = .{
-        .nickname = env_map.get("NICKNAME") orelse {
-            @panic("DISCORD_TOKEN not found in environment variables");
-        },
-        .username = env_map.get("USERNAME") orelse {
-            @panic("USERNAME not found in environment variables");
-        },
-        .realname = env_map.get("REALNAME") orelse {
-            @panic("REALNAME not found in environment variables");
-        },
-        .password = env_map.get("PASSWORD"),
-        .irc_channel = env_map.get("IRC_CHANNEL") orelse {
-            @panic("IRC_CHANNEL  found in environment variables");
-        },
-        .guild_id = try toInt(u64, env_map.get("GUILD_ID") orelse {
-            @panic("GUILD_ID  found in environment variables");
-        }),
-        .server = env_map.get("SERVER") orelse {
-            @panic("SERVER not found in environment variables");
-        },
-        .port = try toInt(u16, env_map.get("PORT") orelse {
-            @panic("PORT not found in environment variables");
-        }),
+fn initIrc(allocator: std.mem.Allocator, envMap: *std.process.EnvMap) !void {
+    const host = getenv(envMap, "HOST");
+    const port = utils.toInt(u16, getenv(envMap, "PORT")) catch |err| {
+        std.debug.print("Invalid port: {}\n", .{err});
+        @panic("PORT environment variable must be a valid u16");
     };
-    defer user.*.deinit();
 
-    channelCache = std.StringHashMap(Discord.Channel).init(allocator);
-    defer channelCache.deinit();
+    ircSession = try allocator.create(Irc.Session);
+    ircSession.* = Irc.init(allocator);
+    defer allocator.destroy(ircSession);
 
-    // IRC
-    const tcp = try std.net.tcpConnectToHost(allocator, user.server, user.port);
-    defer tcp.close();
-
-    var root_ca = try tls.config.cert.fromSystem(allocator);
-    defer root_ca.deinit(allocator);
-
-    connection = try allocator.create(tls.Connection(net.Stream));
-    connection.* = try tls.client(tcp, .{
-        .host = user.server,
-        .root_ca = root_ca,
-        .insecure_skip_verify = true,
+    try ircSession.start(.{
+        .host = host,
+        .port = port,
+        .user = .{
+            .nickname = "b0tm4n",
+            .username = "0xB0tm4n",
+            .channels = &[_][]const u8{ "#test1", "#test2" },
+        },
+        .actions = .{
+            .on_message = ircMessage,
+            .on_join = initChannels,
+        },
     });
-    defer _ = connection.close() catch null;
+}
 
-    // DISCORD
-    const token = env_map.get("DISCORD_TOKEN") orelse {
-        @panic("DISCORD_TOKEN not found in environment variables");
+fn initDiscord(allocator: std.mem.Allocator, envMap: *std.process.EnvMap) !void {
+    const token = getenv(envMap, "DISCORD_TOKEN");
+
+    discordSession = try allocator.create(Discord.Session);
+    discordSession.* = Discord.init(allocator);
+    defer {
+        allocator.destroy(discordSession);
+        discordSession.deinit();
+    }
+
+    const intents = Discord.Intents{
+        .Guilds = true,
+        .GuildMessages = true,
+        .GuildMembers = true,
+        .MessageContent = true,
     };
-    session = try allocator.create(Discord.Session);
-    session.* = Discord.init(allocator);
-    defer session.deinit();
 
-    const intents = comptime blk: {
-        var bits: Discord.Intents = .{};
-        bits.Guilds = true;
-        bits.GuildMessages = true;
-        bits.GuildMembers = true;
-        bits.MessageContent = true;
-        break :blk bits;
-    };
-
-    try session.start(.{
+    try discordSession.start(.{
         .intents = intents,
         .authorization = token,
-        .run = .{
-            .message_create = &send_msg,
-            .ready = &init_client,
-            .channel_delete = &delete_channel,
-            .channel_update = &update_channel,
-        },
+        .run = .{},
         .log = .yes,
         .options = .{},
         .cache = .defaults(allocator),
     });
+}
+
+pub fn main() !void {
+    var single_threaded_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer single_threaded_arena.deinit();
+
+    var thread_safe_arena: std.heap.ThreadSafeAllocator = .{
+        .child_allocator = single_threaded_arena.allocator(),
+    };
+    const allocator = thread_safe_arena.allocator();
+
+    const envMap = try allocator.create(std.process.EnvMap);
+    envMap.* = try std.process.getEnvMap(allocator);
+    defer allocator.destroy(envMap);
+    defer envMap.deinit();
+
+    const IrcThread = try std.Thread.spawn(.{}, initIrc, .{ allocator, envMap });
+    const DiscordThread = try std.Thread.spawn(.{}, initDiscord, .{ allocator, envMap });
+    IrcThread.join();
+    DiscordThread.join();
 }
